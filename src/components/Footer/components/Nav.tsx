63f27ad4f433dea2c6e655728fa6eedba0bd7dc4
korrio <korr@aq1.co>
2021-01-29 18:20:33 +0700

    first init

---
+++ src/components/Footer/components/Nav.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Nav: React.FC = () => {
+  return (
+    <StyledNav>
+      <StyledLink
+        target="_blank"
+        href="https://kovan.etherscan.io/address/0xBd82D8aaF403A866A3241Ae53503A2e5b9E22784"
+      >
+        Phonenix Contract
+      </StyledLink>
+      {/*<StyledLink
+        target="_blank"
+        href="https://uniswap.info/pair/0xce84867c3c02b05dc570d0135103d3fb9cc19433"
+      >
+        Phonenixeth-ETH
+      </StyledLink> */}
+     {/* <StyledLink target="_blank" href="https://discord.gg/sqhXAqc">
+        Discord
+      </StyledLink>
+      <StyledLink target="_blank" href="https://github.com/sushiswap">
+        Github
+      </StyledLink>
+      <StyledLink target="_blank" href="https://twitter.com/sushiswap">
+        Twitter
+      </StyledLink>
+      <StyledLink target="_blank" href="https://medium.com/@sushiswapchef">
+        Medium
+      </StyledLink>*/}
+    </StyledNav>
+  )
+}
+
+const StyledNav = styled.nav`
+  align-items: center;
+  display: flex;
+`
+
+const StyledLink = styled.a`
+  color: ${(props) => props.theme.color.red[300]};
+  padding-left: ${(props) => props.theme.spacing[3]}px;
+  padding-right: ${(props) => props.theme.spacing[3]}px;
+  text-decoration: none;
+  &:hover {
+    color: ${(props) => props.theme.color.red[300]};
+  }
+`
+
+export default Nav
---
+++ src/views/Staking/Staking.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Route, Switch, useRouteMatch} from 'react-router-dom'
+import {useWallet} from 'use-wallet'
+
+import chef from '../../assets/img/logo_ Phoenix_1.png'
+
+import Button from '../../components/Button'
+import Page from '../../components/Page'
+import PageHeader from '../../components/PageHeader'
+import WalletProviderModal from '../../components/WalletProviderModal'
+
+import useModal from '../../hooks/useModal'
+import StakeXSushi from "../StakeXSushi";
+
+const Staking: React.FC = () => {
+  const {path} = useRouteMatch()
+  const {account} = useWallet()
+  const [onPresentWalletProviderModal] = useModal(<WalletProviderModal/>)
+  return (
+    <Switch>
+      <Page>
+        {!!account ? (
+          <>
+            <Route exact path={path}>
+              <PageHeader
+                icon={<img src={chef} height="120"/>}
+                subtitle="Welcome to the Phx Bar, stake Phx to earn Phx."
+                title="Irasshaimase!"
+              />
+            </Route>
+            <StakeXSushi/>
+          </>
+        ) : (
+          <div
+            style={{
+              alignItems: 'center',
+              display: 'flex',
+              flex: 1,
+              justifyContent: 'center',
+            }}
+          >
+            <Button
+              onClick={onPresentWalletProviderModal}
+              text="🔓 Unlock Wallet"
+            />
+          </div>
+        )}
+      </Page>
+    </Switch>
+  )
+}
+
+export default Staking
